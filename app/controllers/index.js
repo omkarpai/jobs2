13833373e6849e773769320e58165a2d8540d6df
omkarpai <51873597+omkarpai@users.noreply.github.com>
2020-10-10 16:01:07 +0530

    minor changes

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,8 +4,7 @@ export default   Controller.extend({
     actions: {
         postToDb (){
             let newRecord = this.store.createRecord('index',{
-                title: 'EmberFire is flaming hot!',
-                body: 'You can store and sync data in realtime without a backend.'
+                startOn: '09-01-2000',
             });
             newRecord.save();
         }
--- app/models/index.js
+++ app/models/index.js
@@ -5,6 +5,5 @@ import Model,{ attr } from '@ember-data/model';
 // }
 
 export default Model.extend({
-    title: attr('string'),
-    body: attr('string')
+    startOn: attr('string'),
   });
