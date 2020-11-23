e40f71d9b65b8ff2843528160251ab53d7cb7a3f
omkarpai <51873597+omkarpai@users.noreply.github.com>
2020-11-23 20:13:39 +0530

    *Added a configuration file for Prettier. *Moved Day of week from job.hbs to month.hbs. *Removed calculation logic for month size from month.js making it purely visual now only tracking skips. *Cleaned up remnants of push based approach from controller index.js. Now uses a getter to calculate month size. *Added new helpers is-equal and is-present used in index.hbs. *Fixed issues with skipping weekends.

--- app/components/month.js
+++ app/components/month.js
@@ -1,67 +1,29 @@
 import Component from '@ember/component';
-import {tracked} from '@glimmer/tracking';
-
-
-
+import { tracked } from '@glimmer/tracking';
 import moment from 'moment';
 
-export default class MonthComponent extends Component{
-@tracked nomDays = null;
-@tracked month=null;
-@tracked year=null;
-@tracked days = [];
-@tracked skippedArray = [];
 
 
-didReceiveAttrs(){
-    //Get properties passed in
-    console.log("recieved attrs")
-    this.month= this.get('month');
-    this.year= this.get('year');
-    this.skippedArray = this.get('skippedArray');
-    
-    this.days = [];
+export default class MonthComponent extends Component {
+    @tracked isSkipped;
 
-    if (this.month && this.year !== null)
-        {
-        
-        let newMoment = moment();
-        newMoment.month(this.month);
-        newMoment.year(this.year);
-        //Calculate number of days in the month for the given month and year
-        this.nomDays = newMoment.daysInMonth();
-
-        for(let i=1 ; i<=this.nomDays; i++)
-        {
-            let isSkipped= false;
-            let skippedRecord = null;
-            let dateString = i + "-" + this.month + "-" + this.year;
-
-            //Iterate over skipped array and find if current day is skipped
-            this.skippedArray.every(
-                (element)=>{
-                    if (dateString === element.skippedDate){
-                        isSkipped = true;
-                        skippedRecord = element;
-                        return false;
-                    }
-                }
-            )
-            //Create an array of days with property of each day.
-            this.days[i-1] = {
-                dayOfMonth:i,
-                isSkipped: isSkipped,
-                fullDate: i + "-" + this.month + "-" +this.year,
-                skippedRecord: skippedRecord
-            }
+    didReceiveAttrs() {
+        if (this.get('skipped') === "True") {
+            this.isSkipped = true;
+        }
+        else {
+            this.isSkipped = false;
         }
     }
+
+
+    get dayOfTheWeek() {
+        let thisMoment = moment(this.get('fullDate'), 'D-MMM-YYYY');
+        return thisMoment.format('dddd');
+    }
+
+
 }
 
-
-
-
-}
-    
 
 
---
+++ app/helpers/is-present.js
@@ -0,0 +1,18 @@
+import { helper } from '@ember/component/helper';
+
+export default helper(function isPresent(args) {
+  let [skippedArray, date] = args;
+  let dateIsPresent = false;
+  skippedArray.forEach(
+    element => {
+      if (element.skippedDate === date) {
+        dateIsPresent = true;
+      }
+    });
+  if (dateIsPresent) {
+    return true
+  }
+  else {
+    return false
+  }
+});
